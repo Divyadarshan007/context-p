842da4da676797b27aa4d0899f5911c3e191e981
divyadarshan007 <dasdivyadarshan@gmail.com>
2025-09-11 14:02:05 +0530

    done with delete and  read operation

--- src/context/LabContextProvider.jsx
+++ src/context/LabContextProvider.jsx
@@ -1,10 +1,14 @@
-import { addDoc, collection } from "firebase/firestore"
-import { createContext } from "react"
+import { addDoc, collection, deleteDoc, doc, getDocs } from "firebase/firestore"
+import { createContext, useEffect, useState } from "react"
 import { db } from "../config/firebase"
 
 export const LabContext = createContext()
 const LabContextProvider = ({ children }) => {
+    const [labs, setLabs] = useState([])
     const collectionRef = collection(db, "labs")
+    useEffect(() => {
+        fetchData()
+    }, [])
     const addLab = async (lab) => {
         try {
             const res = await addDoc(collectionRef, {
@@ -16,8 +20,25 @@ const LabContextProvider = ({ children }) => {
 
         }
     }
+    
+    const fetchData = async () => {
+        const { docs } = await getDocs(collectionRef)
+        let allLabs = docs.map((lab) => {
+            return {
+                id: lab.id,
+                ...lab.data()
+            }
+        })
+        setLabs(allLabs)
+    }
+    
+    const deleteLab = async (labId) => {
+        await deleteDoc(doc(db,"labs", labId)) 
+        fetchData()
+    }
+    
     const value = {
-        addLab
+        addLab, labs, deleteLab
     }
     return (
         <LabContext.Provider value={value}>
